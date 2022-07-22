8e1155e0fcc44cd5850720c9ff7d1e4a5c02e842
Diye Mark <diyye101@gmail.com>
2022-07-22 14:13:49 +0300

    chore: updated dbt

---
+++ dbt/models/dev/open_traffic_details.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table') }}
+
+/*
+    Uncomment the line below to remove records with null `id` values
+    with open_traffic_new as (
+
+    select * from open_traffic
+
+)
+
+select *
+from open_traffic_new
+*/
+
+select * from open_traffic where type = ' Car'
+
